b1e5162a307aee5b0b9f47188c9c48643b85bc7d
Fariepraz <r182265f@students.msu.ac.zw>
2022-06-10 10:04:27 +0200

    first commit

---
+++ project/components/navbar.vue
@@ -0,0 +1,65 @@
+<template>
+<div>
+  
+ 
+  <v-card
+    
+  >
+    <v-toolbar color="green" >
+      <v-app-bar-nav-icon color="white" @click="drawer=!drawer"></v-app-bar-nav-icon>
+
+      <v-toolbar-title class="white--text"> Practice</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+      <span class="white--text"> Log Out</span>
+      <v-btn icon>
+        <v-icon right>mdi-exit-to-app</v-icon>
+      </v-btn>
+    </v-toolbar>
+    <v-navigation-drawer app v-model="drawer" color="grey lighten-3" >
+      <v-list-item>
+        <v-list-item-content>
+        
+        
+        </v-list-item-content>
+      </v-list-item>
+      <v-list
+        dense
+        nav
+      >
+        <v-list-item
+          v-for="item in items"
+          :key="item.title"
+          link
+        >
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+  </v-navigation-drawer>
+  
+  </v-card>
+   </div> 
+</template>
+
+<script>
+  export default {
+    data(){
+        return{
+            drawer: false,
+             items: [
+          { title: 'Dashboard', icon: 'mdi-view-dashboard' },
+          { title: 'Photos', icon: 'mdi-image' },
+          { title: 'About', icon: 'mdi-help-box' },
+        ],
+        right: null,
+        
+        }
+    }
+  }
+</script>
---
+++ project/pages/index.vue
@@ -0,0 +1,116 @@
+<template>
+
+<div>
+  
+<Navbar/>
+
+
+<v-row class=" justify-center mt-10" >
+<v-col md="6">
+ <v-card
+  elevation="12"
+  class="grey lighten-2"
+>
+
+<v-form>
+  <h2 class="indigo darken-4"><center> Registration form</center></h2>
+    <v-container>
+      <v-row>
+        <v-col
+          cols="12"
+          sm="6"
+        >
+          <v-text-field
+            v-model="first"
+            label="First Name"
+            filled
+          ></v-text-field>
+        </v-col>
+
+        <v-col
+          cols="12"
+          sm="6"
+        >
+          <v-text-field
+            v-model="last"
+            label="Last Name"
+            filled
+          ></v-text-field>
+        </v-col>
+      </v-row>
+       <v-row>
+        <v-col
+          cols="12"
+          sm="6"
+        >
+          <v-text-field
+            v-model="email"
+            label="Email Address"
+            filled
+          ></v-text-field>
+        </v-col>
+
+        <v-col
+          cols="12"
+          sm="6"
+        >
+          <v-text-field
+            v-model="number"
+            label="Phone Number"
+            filled
+          ></v-text-field>
+        </v-col>
+          <v-col
+          cols="12"
+          sm="6"
+        >
+          <v-text-field
+            v-model="Age"
+            label="Age"
+            filled
+          ></v-text-field>
+        </v-col>
+      
+        <v-col
+          cols="12"
+          sm="6"
+        >
+          <v-text-field
+            v-model="Address"
+            label="Pysical Address"
+            filled
+          ></v-text-field>
+        </v-col></v-row>
+    </v-container>
+   <v-btn 
+  color="green"
+  depressed
+  elevation="2"
+  tile
+>
+Register</v-btn> 
+    <v-btn
+  color="red"
+  depressed
+  elevation="2"
+  tile
+>
+Cancel</v-btn>
+  </v-form>
+  
+</v-card> 
+</v-col>
+</v-row>
+
+
+</div>
+  
+</template>
+
+<script>
+import Navbar from '../components/navbar.vue';
+export default {
+    name: "IndexPage",
+    components: { Navbar }
+}
+</script>
